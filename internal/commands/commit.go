6cf92c3e195603ded9efab8e457e84e11dce61e4
Javier Tia <floss@jetm.me>
2026-04-08 16:04:02 -0600

    commands/commit: update doc comment for configurable command
    Reference cfg.CommitCmd instead of hardcoded devtool commit.
    
    Signed-off-by: Javier Tia <floss@jetm.me>

--- internal/commands/commit.go
+++ internal/commands/commit.go
@@ -1,6 +1,6 @@
 package commands
 
-// CommitDoneMsg is sent after a devtool commit subprocess exits.
+// CommitDoneMsg is sent after a commit subprocess (cfg.CommitCmd) exits.
 // Both AddModel and HunkAddModel handle this message to refresh state.
 type CommitDoneMsg struct {
 	Err error
